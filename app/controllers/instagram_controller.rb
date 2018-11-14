7f603fc71e38c63ab363f96d97be7c2da11e352b
Julien Gracia <gracia.julien@gmail.com>
2018-11-14 23:14:34 +0100

    response ok from api

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -8,6 +8,28 @@ class InstagramController < ApplicationController
 
   def callback
     # we received a code from instagram API
-    code = params[:code]
+
+    get_token(params[:code])
+    # now we request access_token
+
+  end
+
+  def index
+  end
+
+  private
+
+  def get_token(my_code)
+    url = "https://api.instagram.com/oauth/access_token"
+    body = {
+      client_id: Rails.application.credentials.instagram[:client_id],
+      client_secret: Rails.application.credentials.instagram[:client_secret],
+      grant_type: 'authorization_code',
+      code: my_code
+      redirect_uri: "http://localhost:3000/instagram/index"
+    }
+    p body
+    my_request = HTTParty.post(url, body: body)
+    p my_request
   end
 end
